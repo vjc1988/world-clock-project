f9df9277e4f3bab0ea83f86c357f276e2ab1a773
vjc1988 <victoriajaynecadd@yahoo.com>
2024-04-25 15:56:21 +0100

    Added functionality to the city select element

--- src/index.js
+++ src/index.js
@@ -9,5 +9,26 @@ function currentTime() {
     "HH:mm:ss [<small>]A[</small>]"
   );
 }
+
+function changeCity(event) {
+  let city = event.target.value;
+  let cityName = city.replace("_", " ").split("/")[1];
+  let cityTimeZone = moment().tz(city);
+
+  let citySelect = document.querySelector("#city");
+  citySelect.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTimeZone.format("dddd Do MMMM YYYY")}</div>
+          </div>
+          <div class="time">${cityTimeZone.format(
+            "HH:mm:ss"
+          )} <small>${cityTimeZone.format("A")}</small>
+        </div>`;
+}
+
 currentTime();
 setInterval(currentTime, 1000);
+
+let citySelectElement = document.querySelector("#city-select");
+citySelectElement.addEventListener("change", changeCity);
